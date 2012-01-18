5bda55f9b3f645e74b8638185fb5a027c070c0fb
Winfried Mosler <winfried.mosler@launsch.de>
2012-01-18 22:29:59 +0100

    Initial commit

---
+++ html5boilerplate2.0/js/script.js
@@ -0,0 +1,12 @@
+/* Author: 
+
+*/
+$(document).ready(function() {
+	$("#gallery a").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});
+});
---
+++ html5boilerplate2.0/publish.sh
@@ -0,0 +1,3 @@
+ant -f build/build.xml 
+scp -r publish/  mosler@weisserth.net:/home/www/mosler/htdocs/www.baeckerei-ruehle.de/html5bp_publish_upload
+
